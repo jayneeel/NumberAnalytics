904a35d16a14222f38f947c051f0361313b68610
Jayneel Kanungo <jayneelkanungo57@gmail.com>
2024-06-05 02:39:09 +0530

    - resource drawable refactoring

--- app/src/main/java/com/example/numberanalytics/MainActivity.kt
+++ app/src/main/java/com/example/numberanalytics/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.numberanalytics
 
-import android.opengl.Visibility
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
